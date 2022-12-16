f122c697c7f07c8bcb8db4c7a99a06ab1bf0b63b
abinaditama <abinaditama16oct@gmail.com>
2022-12-16 10:00:47 +0700

    Try looping while

--- Exercise2_Looping.py
+++ Exercise2_Looping.py
@@ -8,9 +8,14 @@ while book_already_read < number_of_books:
 
 jml_buku = 10
 jml_buku_dibaca = 0
+total_baca = 0
 while jml_buku_dibaca < jml_buku:
-    if jml_buku_dibaca == 9:
-        print(f'Buku ke {jml_buku_dibaca} belum dibaca')
+    if jml_buku_dibaca == 10:
+        print(f'Buku ke {jml_buku_dibaca +1 } belum dibaca')
+        while total_baca < 5:
+            print(f'ulangi buku ke {jml_buku_dibaca + 1}')
+            total_baca += 1
+        jml_buku_dibaca += 1
     else:
         jml_buku_dibaca += 1
         print(f'jumlah buku yg sudah dibaca {jml_buku_dibaca}')
