a1ca8ea6dc9c15eae9548de2237aca9871121ad9
abinaditama <abinaditama16oct@gmail.com>
2022-12-20 21:07:30 +0700

    Try looping while

---
+++ Exercise3_List.py
@@ -0,0 +1,48 @@
+# sum all list item
+list1 = [-4,-3,1,5,7,9,-4]
+x = 0
+print(f'List will be added = {list1}')
+for i in list1:
+    x += i
+print(f'addition result is {x}')
+print('\n')
+
+# multiply all the items in a list
+print(f'list to be multiplicated {list1}')
+y = 1
+for i in list1:
+    y = y*i
+print(f'multiplication result is {y}')
+print('\n')
+
+# largest number in list
+print('largest number in list1')
+z = list1[0]
+for i in range(len(list1)):
+    if list1[i] > z:
+        z = list1[i]
+print(f'largest number is {z}')
+print('\n')
+
+# largest number in list
+print('smallest number in list1')
+z = list1[0]
+for i in range(len(list1)):
+    if list1[i] < z:
+        z = list1[i]
+print(f'smallest number is {z}')
+print('\n')
+
+'''count the number of strings where the string 
+length is 2 or more and the first 
+and last character are same from a given list of strings'''
+
+list2 = ['abc', 'xyz', 'aba', '1221']
+print(list2)
+counter = 0
+for i in list2:
+    if len(i)>2:
+        if i[0] == i[-1]:
+            counter +=1
+print(f'string length more than 2 character, first and last char are same {counter}')
+print('\n')
